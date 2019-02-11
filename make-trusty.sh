7ad67693dcb189a2edb6da6f61587773e7a30fd9
Tobias Kuhn <kuhntobias@gmail.com>
2019-02-11 16:23:50 +0100

    Add files for FAIR Principles

--- make-trusty.sh
+++ make-trusty.sh
@@ -5,15 +5,33 @@
 # $ ./make-trusty.sh fair-principles
 #
 
-if [ ! -f nanopub.jar ]; then
-  echo "Downloading nanopub.jar file..."
-  wget -O nanopub.jar -nv https://github.com/Nanopublication/nanopub-java/releases/download/nanopub-1.19/nanopub-1.19-jar-with-dependencies.jar
+USAGE="Usage: $ ./make-trusty.sh fair-principles"
+
+if [ -z $1 ]; then
+  echo $USAGE
+  exit 1
 fi
 
-NP='java -jar nanopub.jar' 
+if [ ! -z $2 ]; then
+  echo $USAGE
+  exit 1
+fi
+
+if [ ! -f $1.trig.pre ]; then
+  echo "File $1.trig.pre does not exist"
+  exit 1
+fi
+
+if [ `command -v np` ]; then
+  NP='np' 
+else
+  if [ ! -f nanopub.jar ]; then
+    echo "Downloading nanopub.jar file..."
+    wget -O nanopub.jar -nv https://github.com/Nanopublication/nanopub-java/releases/download/nanopub-1.19/nanopub-1.19-jar-with-dependencies.jar
+  fi
+  NP='java -jar nanopub.jar' 
+fi
 
 echo "Processing $1.trig.pre..."
-$NP mktrusty $1.trig.pre
+$NP mktrusty -o $1.trig $1.trig.pre
 
-echo "Post-processing result for $1.trig.pre..."
-mv trusty.$1.trig.pre $1.trig
