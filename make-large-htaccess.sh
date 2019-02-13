342cdfd9629f99639607f06e072a272fe14a047c
Tobias Kuhn <kuhntobias@gmail.com>
2019-02-13 16:02:45 +0100

    Make large htaccess file

---
+++ make-large-htaccess.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+USAGE="$ ./make-large-htaccess.sh"
+
+if [ ! -z $1 ]; then
+  echo "Usage: $USAGE"; exit 1
+fi
+
+(
+  cat head.htaccess
+  echo
+  echo
+  cat principles.htaccess
+) \
+  > htaccess
+
