4c69fb5e4521b163d7b53d51595da62c985d2cad
Tobias Kuhn <kuhntobias@gmail.com>
2019-02-14 20:44:30 +0100

    Make index for principles

--- make-trusty.sh
+++ make-trusty.sh
@@ -23,3 +23,13 @@ fi
 echo "Processing $1.trig.pre..."
 $NP mktrusty -o $1.trig $1.trig.pre
 
+echo "Making index..."
+np mkindex \
+  -u https://w3id.org/fair/principles/np/index/ \
+  -a https://doi.org/10.1038/sdata.2016.18 \
+  -c https://orcid.org/0000-0002-1267-0234 \
+  -c https://orcid.org/0000-0003-4727-9435 \
+  -t "Nanopublications representing the FAIR Principles" \
+  -l http://creativecommons.org/licenses/by/4.0/ \
+  -o $1.index.trig \
+  $1.trig
