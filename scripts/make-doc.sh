da87530969104991f0fb868e0fde1fbb1779dfe2
Tobias Kuhn <kuhntobias@gmail.com>
2019-07-08 13:25:24 +0200

    Keep Turtle files

--- scripts/make-doc.sh
+++ scripts/make-doc.sh
@@ -20,8 +20,15 @@ if [ -z "$WIDOCOJAR" ]; then
   exit 1
 fi
 
-$SCRIPTDIR/np op extract -a -o $1.ttl $1.trig
+$SCRIPTDIR/np op extract -a -o $1.ttl.pre $1.trig
+
+(
+  echo "# The content of this file is automatically extracted from $1.trig." ;
+  echo "# Changes should be made in $1.trig.pre." ;
+  echo "";
+  cat $1.ttl.pre
+) > $1.ttl
+
+rm $1.ttl.pre
 
 java -jar $WIDOCOJAR -rewriteAll -ontFile $1.ttl -outFolder doc/$1
-
-rm $1.ttl
